f3a15cf9f80b2814eecfb9e9d8d311c7328e29e9
werderame <werderame@gmail.com>
2025-06-27 10:39:36 +0200

    Formatted and commented for readability

--- op.py
+++ op.py
@@ -1,3 +1,5 @@
+# Optimization Project Main Script
+
 import os
 import importlib
 from modules import recipes, menu, database, purchase, fefo_func as ff, optimize as sol, analyze
@@ -5,15 +7,16 @@ import pandas as pd
 import warnings
 importlib.reload(recipes), importlib.reload(menu), importlib.reload(database) 
 importlib.reload(purchase), importlib.reload(ff), importlib.reload(sol)
-
 warnings.filterwarnings("ignore", category=UserWarning) # Suppress warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+# === DIR SETUP ===
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # Set up paths
 output_dir = os.path.join(base_dir, "Optimization_project", "data", "model_output")
 
 # === SCRAPE RECIPES & CREATE RECIPES ===
-#recipes.generate_hellofresh_bom() 
+# recipes.generate_hellofresh_bom()
+"""Uncomment to scrape today's recipes"""
 menu.plan_menu(min_meals=950, max_meals=1050, weeks=6, start_date='2025-05-01') 
 
 # === LOAD DB DATA ===
@@ -30,12 +33,12 @@ purchase.load_purchases(inventory)
 # === FEFO ALLOCATION ===
 e_1 = 0.00  # Perfect FEFO
 e_2 = 0.03  # Imperfect FEFO
-d, i, w = ff.fefo_daily(demand, inventory, e_1, output_names=['d', 'i', 'w']) # Perfect FEFO Ops
-id, ii, iw = ff.fefo_daily(demand, inventory, e_2, output_names=['id', 'ii', 'iw'])  # Imperfect FEFO Ops # 3% of inventory is replaced in the list randomly
+d, i, w = ff.fefo_daily(demand, inventory, e_1, output_names=['d', 'i', 'w']) # Perfect FEFO Ops: 0% non-adherence
+id, ii, iw = ff.fefo_daily(demand, inventory, e_2, output_names=['id', 'ii', 'iw'])  # Imperfect FEFO: 3% non-adherence
 print('💾 FEFO allocation complete! Output available as CSV.')   
 
 # === OPTIMIZATION ===
-print('Starting to Optimize Inventory')
+print('Starting to Optimize Inventory..')
 demand_suggested = sol.optimize_inventory(gap=0.05)
 sol.publish_solution(demand_suggested)
 
@@ -45,3 +48,5 @@ r2 = analyze.fefo_results('id', 'iw', 'Imperfect FEFO', e_2)
 r3 = analyze.opt_results('d', 'w')
 print('\n🧩 Results of Optimization:')
 print(pd.concat([r1, r2, r3], ignore_index=True))
+
+# end of script
